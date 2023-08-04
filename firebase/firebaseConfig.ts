0aacb6acdbd5070ec4dca912d1d413b02add703a
Fahimansari <fahimpjan@gmail.com>
2023-08-05 00:19:21 +0530

    Added the Sign In template, configured firebase authentication

---
+++ firebase/firebaseConfig.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxnMRnK5g4CoG4FoP_1dJV2jNV4XuYIxs",
+  authDomain: "flickfindz.firebaseapp.com",
+  projectId: "flickfindz",
+  storageBucket: "flickfindz.appspot.com",
+  messagingSenderId: "665817482787",
+  appId: "1:665817482787:web:27a14963fbaab7f0365396"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+export const initFirebase = () =>{
+    return app;
+}
