0db6906e0032c569b612b9ec06a475b59800ddcd
Fahimansari <fahimpjan@gmail.com>
2023-08-06 13:47:48 +0530

    Added Sign Out in the App

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,37 @@
+'use client'
+import { initFirebase } from '@/firebase/firebaseConfig';
+import { useAuthState } from 'react-firebase-hooks/auth';
+// import { error } from 'console';
+import { getAuth, signOut } from 'firebase/auth';
+import { useRouter } from 'next/navigation';
+
 
 const SignedIn = () => {
+  const app = initFirebase();
+  const auth = getAuth();
+
+  const [user, loading] = useAuthState(auth)
+  if (!user) {
+    return <>
+    <div>Signed Out</div>
+    </>
+  }
+  const router = useRouter();
+  const handlesignOut = () => {
+    signOut(auth). then(() => {
+      console.log(auth.currentUser);
+      
+      // router.push('/')
+    }).catch((error) => {
+      console.log(error);
+      
+    })
+  }
   return (
+  <>
     <div>SignedIn</div>
+    <button onClick={handlesignOut}>SignOut</button>
+  </>
   )
 }
 
