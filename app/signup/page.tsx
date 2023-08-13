a73e2e28907ee3c0b9292380682984162b23f9b7
Fahimansari <fahimpjan@gmail.com>
2023-08-13 22:17:37 +0530

    Working on the Sign Up

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -33,11 +33,11 @@ const handleSignUp = () => {
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <img
               className="mx-auto h-10 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt="Your Company"
+              src="/favicon.ico"
+              alt="Flick Findz"
             />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Sign in to your account
+            <h2 className="mt-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Sign up with Flick Findz
             </h2>
           </div>
   
