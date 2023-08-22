66ea053cf8192a357bc2d94a9e9ea83a990ddf53
Fahimansari <fahimpjan@gmail.com>
2023-08-22 09:27:19 +0530

    Cleaned up the UI of Submit button

--- components/QuestionForm.tsx
+++ components/QuestionForm.tsx
@@ -2,21 +2,35 @@ import questions_list from "@/constants/question_list";
 import Question from "./Question";
 
 const QuestionForm = () => {
+  console.log(questions_list);
 
-    console.log(questions_list);
-    
-
-    const handleSubmit = () => {
-      console.log(`This function handles the submit`);
-      
-    }
+  const handleSubmit = () => {
+    console.log(`This function handles the submit`);
+  };
   return (
     <>
-    <form className="relative bg-white px-6 pb-8 pt-10 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-1-2 sm:rounded-lg sm:px-10 w-1/2" action='#' method=''>
-        { questions_list.map(question => {return <Question key={question.id} question={question} /> })}
-        <button type="submit" onClick={handleSubmit}>Submit</button>
-    </form>
-    </>  )
-}
+      <form
+        className='relative bg-white px-6 pb-8 pt-10 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-1-2 sm:rounded-lg sm:px-10 w-1/2'
+        action='#'
+        method=''
+      >
+        {questions_list.map((question) => {
+          return <Question key={question.id} question={question} />;
+        })}
+        <div className='flex justify-center items-center mt-10 p-10'>
+          {" "}
+          {/* Adjust this container's classes as needed */}
+          <button
+            type='submit'
+            className='bg-indigo-500 hover:bg-indigo-600 text-white font-semibold py-2 px-7 rounded focus:outline-none focus:ring focus:ring-indigo-200 shadow-xl'
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </>
+  );
+};
 
-export default QuestionForm
+export default QuestionForm;
