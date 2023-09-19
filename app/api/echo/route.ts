85e2e239775db26e823da7f2dbd76165e7b1b355
Fahimansari <fahimpjan@gmail.com>
2023-09-19 23:43:16 +0530

    Playing around the API Routes

--- app/api/echo/route.ts
+++ app/api/echo/route.ts
@@ -1,9 +1,13 @@
 import { NextResponse } from 'next/server';
 
+const OPENAI_KEY = process.env.OPENAI_API_KEY
+
 export async function GET(request:Request) {
     console.log(request);
     const samp_res = request.body
     const {searchParams} = new URL(request.url)
+
+    console.log(OPENAI_KEY)
     console.log(searchParams);
 
     return NextResponse.json({samp_res})
@@ -13,8 +17,11 @@ export async function GET(request:Request) {
 
 export async function POST(request:Request) {
     // const data = await request.formData()
-    const data = await request.json()
+    // const data = await request.json()
+    const data = await request.formData()
+    console.log(OPENAI_KEY);
     console.log(data);
+    
     
     const samp_res = request.body
     const {searchParams} = new URL(request.url)
