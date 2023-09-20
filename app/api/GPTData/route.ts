c65a9a59d98a6c67950d2a58d78a05524b0919db
Fahimansari <fahimpjan@gmail.com>
2023-09-20 21:49:10 +0530

    Made minor changes

--- app/api/GPTData/route.ts
+++ app/api/GPTData/route.ts
@@ -1,1 +1,5 @@
-console.log(`This is a dummy`);
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+    return NextResponse.json({message: "Hi there"})
+}
