54041e0471411ec33192a263be44107f5a87ebf6
Fahimansari <fahimpjan@gmail.com>
2023-09-20 19:54:34 +0530

    Playing around with API Routes

--- app/api/echo/route.ts
+++ app/api/echo/route.ts
@@ -16,18 +16,52 @@ export async function GET(request:Request) {
 }
 
 export async function POST(request:Request) {
-    // const data = await request.formData()
     // const data = await request.json()
-    const data = await request.formData()
-    console.log(OPENAI_KEY);
-    console.log(data);
+   
+    // const data_body = await request.body;
+    const data_form = await request.formData()
+    const  parsedData = data_form.get('test')
+    
+    // const parsedDataAll = data_form.forEach((data) => {
+    //     console.log(data);
+        
+    // })
+
+
+    const parsedDataAll = data_form.entries()
+    // console.log(parsedDataAll);
+    
+    for (var pair of data_form.entries()) {
+        console.log("Key: " + pair[0] + ", Value: " + pair[1]);
+      }
+
+
+    // for (const [key, value] of parsedDataAll) {
+    //     console.log(`Key: ${key}, Value: ${value}`);
+    //   }
+
+    // console.log(parsedData);
+
+    // console.log(parsedDataAll);
+
+    
+    console.log(data_form);
     
     
-    const samp_res = request.body
+
+    
+   
+    // console.log(OPENAI_KEY);
+    // console.log(data);
+    
+    // console.log(request);
+    
+    const form_data = new FormData()
     const {searchParams} = new URL(request.url)
     console.log(searchParams);
 
-    return NextResponse.json({samp_res})
+    return NextResponse.json(data_form)
+    
 
     
 }
