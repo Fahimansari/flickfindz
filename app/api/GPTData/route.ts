54df72e79b0044110f052fd490da58b4dd07d379
Fahimansari <fahimpjan@gmail.com>
2023-09-24 23:45:15 +0530

    Trying to parse the string that we get from chat gpt to an array

--- app/api/GPTData/route.ts
+++ app/api/GPTData/route.ts
@@ -5,13 +5,15 @@ const OPENAI_API_KEY = process.env.OPENAI_API_KEY
 // const PORT = 
 
 export async function GET(request: NextRequest) {
-    
-    const prompt: string = 'Give me something back'     // replace this with the client entry about the film
-
 
 
     const { searchParams } = new URL(request.url)
     let params: { [key: string]: string } = {}
+    searchParams.forEach((value, key) => {
+        params[key] = value;
+    })
+
+    const prompt: string = `Give me a list of 3 movie genres as ${params.movieGenre}, preferred movie setting as ${params.movieSetting}, pace of movie as ${params.moviePace}, preferred movie tone as ${params.movieTone}, preferred parental guidance as ${params.movieRating}, preferred movie story line as ${params.movieStory} and show these as an array. The response should be strictly an array of 3 movies without any others characters or numberings. For example, ['Batman', 'The Matrix', 'John Wick']`     // replace this with the client entry about the film
 
     const openai = new OpenAI({
         apiKey: OPENAI_API_KEY
@@ -29,15 +31,20 @@ export async function GET(request: NextRequest) {
 
     })
 
+
+    const cleanedResponse = JSON.parse(response.choices[0].text.replace(/[^\w\s,]/g, ''));
+    const stringified = JSON.stringify(response.choices[0].text)
+    const parsedString = JSON.parse(stringified)
+    // const parsedArray = JSON.parse(parsedString)
     console.log(response.choices[0].text);
-    
-    searchParams.forEach((value, key) => {
-        params[key] = value;
-    })
 
+
+
+
+    // console.log(typeof(parsedString))
     console.log(params);
-    
 
 
-    return NextResponse.json({text: response.choices[0].text})
+
+    return NextResponse.json(parsedString)
 }
